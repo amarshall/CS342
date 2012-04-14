c053294d9aa3a38e30f7f60ab484b88dc397a947
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-04-14 18:15:02 -0400

    Add (empty) text files for faq data

--- Project3/src/customerService/product/FAQFiles.java
+++ Project3/src/customerService/product/FAQFiles.java
@@ -7,21 +7,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public enum FAQFiles {
-  SSD_PRODUCT_1 ("ssd1faqs.txt"),
-  SSD_PRODUCT_2 ("ssd2faqs.txt"),
-  SSD_PRODUCT_3 ("ssd3faqs.txt"),
+  SSD_PRODUCT_1 ("data/ssd1faqs.txt"),
+  SSD_PRODUCT_2 ("data/ssd2faqs.txt"),
+  SSD_PRODUCT_3 ("data/ssd3faqs.txt"),
 
-  HDD_PRODUCT_1 ("hdd1faqs.txt"),
-  HDD_PRODUCT_2 ("hdd2faqs.txt"),
-  HDD_PRODUCT_3 ("hdd3faqs.txt"),
+  HDD_PRODUCT_1 ("data/hdd1faqs.txt"),
+  HDD_PRODUCT_2 ("data/hdd2faqs.txt"),
+  HDD_PRODUCT_3 ("data/hdd3faqs.txt"),
 
-  OPTICAL_PRODUCT_1 ("optical1faqs.txt"),
-  OPTICAL_PRODUCT_2 ("optical2faqs.txt"),
-  OPTICAL_PRODUCT_3 ("optical3faqs.txt"),
+  OPTICAL_PRODUCT_1 ("data/optical1faqs.txt"),
+  OPTICAL_PRODUCT_2 ("data/optical2faqs.txt"),
+  OPTICAL_PRODUCT_3 ("data/optical3faqs.txt"),
 
-  BOOK_PRODUCT_1 ("book1faqs.txt"),
-  BOOK_PRODUCT_2 ("book2faqs.txt"),
-  BOOK_PRODUCT_3 ("book3faqs.txt");
+  BOOK_PRODUCT_1 ("data/book1faqs.txt"),
+  BOOK_PRODUCT_2 ("data/book2faqs.txt"),
+  BOOK_PRODUCT_3 ("data/book3faqs.txt");
 
   private String filename;
 
