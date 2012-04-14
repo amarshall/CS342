1b4328abe4e4b3110d1fd1a4838146a87e3fa46b
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-04-14 18:51:03 -0400

    Exact match searching

--- Project3/src/customerService/search/ExactMatchSearch.java
+++ Project3/src/customerService/search/ExactMatchSearch.java
@@ -2,9 +2,17 @@ package customerService.search;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ExactMatchSearch implements SearchStrategy {
   public Set<String> search(String query, List<String> items) {
-    return null;
+    Set<String> results = new TreeSet<String>();
+    for(String str : items) {
+      if(str.matches("(.*)(\\A|\\s)(" + query + ")(\\Z|\\s)(.*)")) {
+        results.add(str);
+      }
+    }
+
+    return results;
   }
 }
