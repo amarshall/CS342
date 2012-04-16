1bc936153b5323156a2a0cf50d820f727c3461fc
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-04-16 18:09:05 -0400

    Whitespace

--- Project3/src/customerService/util/Debug.java
+++ Project3/src/customerService/util/Debug.java
@@ -1,20 +1,20 @@
-
 package customerService.util;
 
 public class Debug {
-	private static int DEBUG_VALUE;
-	public static void setDebug(int debug) {
-        if(debug > 4) {
-            System.err.println("Invalid Debug Value");
-            System.exit(1);
-        }
-        else {
-            DEBUG_VALUE = debug;
-        }
+  private static int DEBUG_VALUE;
+
+  public static void setDebug(int debug) {
+    if(debug > 4) {
+      System.err.println("Invalid debug value");
+      System.exit(1);
+    } else {
+      DEBUG_VALUE = debug;
     }
-	public static void log(int debugCompare, String message) {
-		if(DEBUG_VALUE >= debugCompare) {
-			System.out.println(message);
-		}
-	}
+  }
+
+  public static void log(int debugCompare, String message) {
+    if(DEBUG_VALUE >= debugCompare) {
+      System.out.println(message);
+    }
+  }
 }
